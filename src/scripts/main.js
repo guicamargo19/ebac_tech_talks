83963e4c63f23d85c8a49e2c9516f9a6cdf45d24
camargo <gui.ferreira.camargo@gmail.com>
2024-01-02 16:46:24 -0300

    Contador em JavaScript atualizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Jan 2, 2024 16:44:00");
+const dataDoEvento = new Date("Jul 2, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
